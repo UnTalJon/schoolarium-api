13605ce96b779d70d1473f4ba8d196455468b273
Jon Luna <rght_jon@outlook.es>
2025-09-24 13:37:01 -0600

    add: create base entities to work with the API
    - create Person Entity with basic information
    - create Student Entity with a foreign key that refers on Person,
      relation one to one

---
+++ src/main/kotlin/xyz/xjnt/schoolarium_api/business/model/Person.kt
@@ -0,0 +1,20 @@
+package xyz.xjnt.schoolarium_api.business.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "persons")
+data class Person(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    val name: String,
+
+    val firstSurname: String,
+
+    val secondSurname: String,
+)
