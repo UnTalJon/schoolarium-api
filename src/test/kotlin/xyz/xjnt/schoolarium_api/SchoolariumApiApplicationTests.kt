85505ee8e939654b34ee1be907fbe36d7c7552c9
Jon Luna <rght_jon@outlook.es>
2025-09-24 00:20:52 -0600

    base project

---
+++ src/test/kotlin/xyz/xjnt/schoolarium_api/SchoolariumApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.xjnt.schoolarium_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolariumApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
