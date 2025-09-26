1797c3732cadda3d3fc88176a6d17c5c62d5c013
Jon Luna <rght_jon@outlook.es>
2025-09-25 22:40:54 -0600

    add: JacksonConfig for JsonNullable support
    - update build.gradle.kts to exclude mockito-core
    - add springmockk dependency
    - add jackson-databind-nullable

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,17 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 
     // Custom dependencies
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.7") // Partial updates support
 }
 
 kotlin {
